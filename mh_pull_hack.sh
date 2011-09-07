97ac5e611c30c7d70afeb60bb32a91e6153ad22d
Michael Higgins <mhiggins@puppetlabs.com>
2011-09-07 13:12:13 -0700

    fixed if 'LATEST' is latest, exit and not to pull xxx.all.tar

--- mh_pull_hack.sh
+++ mh_pull_hack.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-wget -O LATEST http://pluto.puppetlabs.lan/dists/LATEST 2>/dev/null 
+
+wget -O LATEST.new http://pluto.puppetlabs.lan/dists/LATEST 2>/dev/null 
+
+if [ $( cat LATEST ) == $( cat LATEST.new ) ] ; then
+echo "got the latest already, exiting"; exit ;
+fi
+mv LATEST.new LATEST
 export LATEST=`cat LATEST`
-wget -O - http://pluto.puppetlabs.lan/dists 2>/dev/null | perl -e 'while (<>){ print if (s/.*(puppet-enterprise-\Q$ENV{LATEST}\E.*.tar).*/$1/)}' | while read tarball ;
-do echo wget http://pluto.puppetlabs.lan/dists/${tarball} ;
+wget -O - http://pluto.puppetlabs.lan/dists 2>/dev/null | perl -e 'while (<>){ print if (s/.*(puppet-enterprise-\Q$ENV{LATEST}\E.*(?<!all)\.tar).*/$1/)}' | while read tarball ;
+do wget -nc http://pluto.puppetlabs.lan/dists/${tarball} ;
 done
