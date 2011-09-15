079d3f34da1df857bfa7c7529fdfa0b800380574
Michael Higgins <mhiggins@puppetlabs.com>
2011-09-15 14:54:46 -0700

    adding fixes and scripts

--- mh_pull_hack.sh
+++ mh_pull_hack.sh
@@ -1,12 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
+cd /opt/enterprise/dists
 wget -O LATEST.new http://pluto.puppetlabs.lan/dists/LATEST 2>/dev/null 
 
-if [ $( cat LATEST ) == $( cat LATEST.new ) ] ; then
-echo "got the latest already, exiting"; exit ;
+
+if [ ` cat LATEST ` == ` cat LATEST.new ` ] ; then
+  rm LATEST.new
+  echo "got the latest already, exiting"
+  exit
 fi
+
 mv LATEST.new LATEST
 export LATEST=`cat LATEST`
+rm puppet-enterprise*.tar
 wget -O - http://pluto.puppetlabs.lan/dists 2>/dev/null | perl -e 'while (<>){ print if (s/.*(puppet-enterprise-\Q$ENV{LATEST}\E.*(?<!all)\.tar).*/$1/)}' | while read tarball ;
 do wget -nc http://pluto.puppetlabs.lan/dists/${tarball} ;
 done
+
+# chgrp developers *
+chmod 0664 LATEST *.tar
